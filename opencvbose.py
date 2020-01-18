c1373403e4b523d7c7b4a444484f322e7ba80ef1
afreglett <a.freglette17@gmail.com>
2020-01-17 23:00:06 -0500

    working (kinda)

--- opencvbose.py
+++ opencvbose.py
@@ -33,11 +33,13 @@ import time
 import glob
 import os
 import Update_Model
+
 video_capture = cv2.VideoCapture(0)
 facecascade = cv2.CascadeClassifier("/Library/Python/2.7/site-packages/cv2/data/haarcascade_frontalface_default.xml")
-fishface = cv2.createFisherFaceRecognizer()
+fishface = cv2.face.FisherFaceRecognizer_create()
+
 try:
-    fishface.load("/Library/Python/2.7/site-packages/cv2/data/trained_emoclassifier.xml")
+    fishface.read("trained_emoclassifier.xml")
 except:
     print("no xml found. Using --update will create one.")
 parser = argparse.ArgumentParser(description="Options for the emotion-based music player") #Create parser object
@@ -61,10 +63,10 @@ def update_model(emotions):
     print("Done!")
 def check_folders(emotions): #check if folder infrastructure is there, create if absent
     for x in emotions:
-        if os.path.exists("dataset\\%s" %x):
+        if os.path.exists("dataset/%s" %x):
             pass
         else:
-            os.makedirs("dataset\\%s" %x)
+            os.makedirs("dataset/%s" %x)
 def save_face(emotion):
     print("\n\nplease look " + emotion + " when the timer expires and keep the expression stable until instructed otherwise.")
     for i in range(0,5):#Timer to give you time to read what emotion to express
@@ -73,14 +75,14 @@ def save_face(emotion):
     while len(facedict.keys()) < 16: #Grab 15 images for each emotion
         detect_face()
     for x in facedict.keys(): #save contents of dictionary to files
-        cv2.imwrite("dataset\\%s\\%s.jpg" %(emotion, len(glob.glob("dataset\\%s\\*" %emotion))), facedict[x])
+        cv2.imwrite("dataset/%s/%s.jpg" %(emotion, len(glob.glob("dataset/%s/*" %emotion))), facedict[x])
     facedict.clear() #clear dictionary so that the next emotion can be stored
 def recognize_emotion():
     predictions = []
     confidence = []
     for x in facedict.keys():
         pred, conf = fishface.predict(facedict[x])
-        cv2.imwrite("images\\%s.jpg" %x, facedict[x])
+        cv2.imwrite("images/%s.jpg" %x, facedict[x])
         predictions.append(pred)
         confidence.append(conf)
     print("I think you're %s" %emotions[max(set(predictions), key=predictions.count)])
@@ -98,11 +100,29 @@ def detect_face():
         return faceslice
     else:
         print("no/multiple faces detected, passing over frame")
+        
 while True:
+
+    ret, frame = video_capture.read() #Grab frame from webcam. Ret is 'true' if the frame was successfully grabbed.
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Convert image to grayscale to improve detection speed and accuracy
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    clahe_image = clahe.apply(gray)
+    #Run classifier on frame
+    face = facecascade.detectMultiScale(clahe_image, scaleFactor=1.1, minNeighbors=15, minSize=(10, 10), flags=cv2.CASCADE_SCALE_IMAGE)
+    for (x, y, w, h) in face: #Draw rectangle around detected faces
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2) #draw it on "frame", (coordinates), (size), (RGB color), thickness 2
+    if len(face) == 1: #Use simple check if one face is detected, or multiple (measurement error unless multiple persons on image)
+        faceslice = crop_face(gray, face) #slice face from image
+##        cv2.imshow("detect", faceslice) #display sliced face
+    else:
+        print("no/multiple faces detected, passing over frame")
+    cv2.imshow("webcam", frame) #Display frame
+    if cv2.waitKey(1) & 0xFF == ord('q'): #imshow expects a termination definition to work correctly, here it is bound to key 'q'
+        break
+    
     detect_face()
     if args.update: #If update flag is present, call update function
         update_model(emotions)
         break
     elif len(facedict) == 10: #otherwise it's regular a runtime, continue normally with emotion detection functionality
         recognize_emotion()
-        break
